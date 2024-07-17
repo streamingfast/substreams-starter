729a903a55e4498c24e2eca27e6c967ed3c43339
arnaudberger <arnaudberger84@gmail.com>
2024-07-17 15:19:16 -0400

    update starter

--- .vscode/streamingfast.substreams/extension.js
+++ .vscode/streamingfast.substreams/extension.js
@@ -17,7 +17,7 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('substreams.helloWorld', function () {
+	const disposable = vscode.commands.registerCommand('substreams-sf.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
