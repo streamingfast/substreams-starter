45fef6377f157471dbf90de8ccf9ed51abd7ee89
Alexandre Bourget <alex@streamingfast.io>
2024-07-17 18:19:20 -0400

    Implement something when you want to `build`.
    Added `auth` to the walkthrough.

--- .vscode/streamingfast.substreams/extension.js
+++ .vscode/streamingfast.substreams/extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const path = require('path');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,22 +10,72 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    const dosomething = function () {
-        vscode.tasks.executeTask(new vscode.Task({
-            type: 'shell',
-            command: 'echo hello world',
-            problemMatcher: [],
-            presentation: {
-                reveal: 'always',
-                panel: vscode.TaskPanelKind.Shared,
-                showReuseMessage: true,
-                clear: true
+	context.subscriptions.push(vscode.commands.registerCommand('substreams.runInit', async function() {
+        vscode.tasks.executeTask(new vscode.Task(
+            {
+                type: 'shell',
+                presentation: {
+                    reveal: 'always',
+                    panel: vscode.TaskPanelKind.Dedicated,
+                    showReuseMessage: false,
+                    clear: true
+                }
             },
-            name: 'Build Substreams Package'
-        }, vscode.TaskScope.Workspace, 'Build Substreams Package', 'substreams'));
-	}
-	context.subscriptions.push(vscode.commands.registerCommand('substreams.build', dosomething));
-	context.subscriptions.push(vscode.commands.registerCommand('substreams.runInit', dosomething));
+            vscode.TaskScope.Workspace,
+            'Init package from generators',
+            'substreams',
+            new vscode.ShellExecution('substreams init')
+        ));
+    }));
+	context.subscriptions.push(vscode.commands.registerCommand('substreams.build', async function() {
+        const activeEditor = vscode.window.activeTextEditor;
+        if (!activeEditor) {
+            vscode.window.showInformationMessage('Build command available when editing a file below your `substreams.yaml` manifest');
+            return
+        }
+        const fileExists = await vscode.workspace.fs.stat(activeEditor.document.uri);
+        if (fileExists) {
+            // File exists
+        } else {
+            // File does not exist
+        }
+
+        // TODO: extract to some other file.
+        const getParentFolderWithFile = async function(currentFilePath, matchingFileName) {
+            let currentDir = path.dirname(currentFilePath);
+            const root = vscode.Uri.file("/")
+            while (true) {
+                const thisLevelFile = vscode.Uri.joinPath(vscode.Uri.file(currentDir), matchingFileName)
+                if (thisLevelFile === root) {
+                    return null
+                }
+                console.log("Looking for this file", thisLevelFile);
+                const fileExists = await vscode.workspace.fs.stat(thisLevelFile)
+                if (fileExists) {
+                    return currentDir
+                } else {
+                    currentDir = path.dirname(currentDir)
+                    continue
+                }
+            }
+        }
+
+        const parentFolder = await getParentFolderWithFile(activeEditor.document.uri.fsPath, "substreams.yaml")
+        if (parentFolder === null) {
+            vscode.window.showInformationMessage('Build command available when editing a file below your `substreams.yaml` manifest');
+            return        
+        }
+        vscode.tasks.executeTask(new vscode.Task(
+            {type: 'shell'},
+            vscode.TaskScope.Workspace,
+            'Build Substreams Package',
+            'substreams',
+            new vscode.ShellExecution('cargo build --target wasm32-unknown-unknown --release', {cwd: parentFolder})
+        ))
+    }))
+	context.subscriptions.push(vscode.commands.registerCommand('substreams.newBlankModule', async function() {}));
+	context.subscriptions.push(vscode.commands.registerCommand('substreams.openRepository', async function() {}));
+	context.subscriptions.push(vscode.commands.registerCommand('substreams.buildFromExistingPackage', async function() {}));
 
 
     vscode.commands.executeCommand(
