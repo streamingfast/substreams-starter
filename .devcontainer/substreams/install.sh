62ac7d522d0e42764fde72522229ac45c1c4355b
Alexandre Bourget <alex@streamingfast.io>
2024-07-16 20:51:01 -0400

    Workable Dockerfile. Improved .devcontainer. Docker-compose with all booting. Feature that installs `graph` CLI

--- .devcontainer/substreams/install.sh
+++ .devcontainer/substreams/install.sh
@@ -8,3 +8,5 @@ mv substreams /usr/bin/substreams
 LINK=$(curl -s https://api.github.com/repos/bufbuild/buf/releases/latest | awk "/download.url.*buf-Linux-$(uname -m)\"/ {print \$2}" | sed 's/"//g')
 echo Downloading $LINK
 curl -L $LINK -o /usr/bin/buf
+
+npm install -g @graphprotocol/graph-cli
