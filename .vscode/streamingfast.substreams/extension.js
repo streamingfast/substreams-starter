d15a5442845fe0db61c43736347c9752e08d3195
Alexandre Bourget <alex@streamingfast.io>
2024-07-26 01:28:44 -0400

    Improve extension.. getting there! Bumped it

--- .vscode/streamingfast.substreams/extension.js
+++ .vscode/streamingfast.substreams/extension.js
@@ -3,8 +3,44 @@
 const vscode = require('vscode');
 const path = require('path');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// TODO: extract to some other file.
+const getParentFolderWithFile = async function(currentFilePath, matchingFileName) {
+    let currentDir = path.dirname(currentFilePath);
+    while (true) {
+        const thisLevelFile = vscode.Uri.joinPath(vscode.Uri.file(currentDir), matchingFileName)
+        console.log("Looking for this file", thisLevelFile);
+        try {
+            const fileExists = await vscode.workspace.fs.stat(thisLevelFile)
+            return currentDir
+        } catch (e) {
+            currentDir = path.dirname(currentDir)
+            console.log("comparing", currentDir.toString(), "/")
+            if (currentDir.toString() === "/") {
+                return null
+            }
+            continue
+        }
+    }
+}
+
+const substreamsModuleSubfolderError = 'Run command on file inside a project with `substreams.yaml`'
+
+async function findSubstreamsProjectRoot(vscode) {
+    const activeEditor = vscode.window.activeTextEditor;
+    if (!activeEditor) {
+        vscode.window.showInformationMessage(substreamsModuleSubfolderError);
+        return
+    }
+
+    const parentFolder = await getParentFolderWithFile(activeEditor.document.uri.fsPath, "substreams.yaml")
+    if (parentFolder === null) {
+        vscode.window.showInformationMessage(substreamsModuleSubfolderError);
+        return        
+    }
+
+    return parentFolder
+}
+
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -27,55 +63,99 @@ function activate(context) {
             new vscode.ShellExecution('substreams init')
         ));
     }));
+
 	context.subscriptions.push(vscode.commands.registerCommand('substreams.build', async function() {
-        const activeEditor = vscode.window.activeTextEditor;
-        if (!activeEditor) {
-            vscode.window.showInformationMessage('`substreams.yaml` not found below currently opened file.');
+        const projectFolder = await findSubstreamsProjectRoot(vscode)
+        if (!projectFolder) {
             return
-        }
-        const fileExists = await vscode.workspace.fs.stat(activeEditor.document.uri);
-        if (fileExists) {
-            // File exists
-        } else {
-            // File does not exist
-        }
-
-        // TODO: extract to some other file.
-        const getParentFolderWithFile = async function(currentFilePath, matchingFileName) {
-            let currentDir = path.dirname(currentFilePath);
-            const root = vscode.Uri.file("/")
-            while (true) {
-                const thisLevelFile = vscode.Uri.joinPath(vscode.Uri.file(currentDir), matchingFileName)
-                if (thisLevelFile === root) {
-                    return null
-                }
-                console.log("Looking for this file", thisLevelFile);
-                const fileExists = await vscode.workspace.fs.stat(thisLevelFile)
-                if (fileExists) {
-                    return currentDir
-                } else {
-                    currentDir = path.dirname(currentDir)
-                    continue
-                }
-            }
-        }
-
-        const parentFolder = await getParentFolderWithFile(activeEditor.document.uri.fsPath, "substreams.yaml")
-        if (parentFolder === null) {
-            vscode.window.showInformationMessage('`substreams.yaml` not found below currently opened file.');
-            return        
         }
         vscode.tasks.executeTask(new vscode.Task(
             {type: 'shell'},
             vscode.TaskScope.Workspace,
             'Build Substreams Package',
             'substreams',
-            new vscode.ShellExecution('cargo build --target wasm32-unknown-unknown --release', {cwd: parentFolder})
+            new vscode.ShellExecution('cargo build --target wasm32-unknown-unknown --release', {cwd: projectFolder})
         ))
     }))
 	context.subscriptions.push(vscode.commands.registerCommand('substreams.newBlankModule', async function() {}));
 	context.subscriptions.push(vscode.commands.registerCommand('substreams.openRepository', async function() {}));
-	context.subscriptions.push(vscode.commands.registerCommand('substreams.buildFromExistingPackage', async function() {}));
+	context.subscriptions.push(vscode.commands.registerCommand('substreams.initSubgraph', async function() {
+        const projectFolder = await findSubstreamsProjectRoot(vscode)
+        if (!projectFolder) {
+            return
+        }        
+        vscode.tasks.executeTask(new vscode.Task(
+            {type: 'shell'},
+            vscode.TaskScope.Workspace,
+            'Initialize subgraph',
+            'substreams',
+            new vscode.ShellExecution('substreams codegen subgraph', {cwd: projectFolder})
+        ))
+    }));
+	context.subscriptions.push(vscode.commands.registerCommand('substreams.initSql', async function() {
+        const projectFolder = await findSubstreamsProjectRoot(vscode)
+        if (!projectFolder) {
+            return
+        }        
+        vscode.tasks.executeTask(new vscode.Task(
+            {type: 'shell'},
+            vscode.TaskScope.Workspace,
+            'Initialize Substreams:SQL',
+            'substreams',
+            new vscode.ShellExecution('substreams codegen subgraph', {cwd: projectFolder})
+        ))
+
+    }));
+
+	context.subscriptions.push(vscode.commands.registerCommand('substreams.runGui', async function() {
+        const projectFolder = await findSubstreamsProjectRoot(vscode)
+        if (!projectFolder) {
+            return
+        }        
+        vscode.tasks.executeTask(new vscode.Task(
+            {type: 'shell'},
+            vscode.TaskScope.Workspace,
+            'Substreams GUI',
+            'substreams',
+            new vscode.ShellExecution('substreams gui', {cwd: projectFolder})
+        ))
+    }));
+
+    async function getPackageList(keywords) {
+        return [{name: "sreamingfast/uniswap-v3"}, {name: "streamingfast/" + keywords}, {name: "streamingfast/solana-explorer"}, {name: "streamingfast/ethereum-explorer"}]
+        // try {
+        //     const response = await axios.get('https://api.substreams.dev/packages');
+        //     return response.data;
+        // } catch (error) {
+        //     console.error('Error fetching package list:', error);
+        //     return [];
+        // }
+    }
+
+    context.subscriptions.push(vscode.commands.registerCommand('substreams.loadFromRegistry', async function() {
+
+        const quickPick = vscode.window.createQuickPick();
+
+        let packageList = []
+        async function refreshList(search) {
+            packageList = await getPackageList(search);
+            quickPick.items = packageList.map(pkg => ({ label: pkg.name }));
+        }
+        refreshList("")
+        quickPick.onDidChangeValue(async value => {
+            refreshList(value)
+        });
+        quickPick.onDidChangeSelection(selection => {
+            vscode.tasks.executeTask(new vscode.Task(
+                {type: 'shell'},
+                vscode.TaskScope.Workspace,
+                'Download Package',
+                'substreams',
+                new vscode.ShellExecution(`curl -O https://spkg.io/${selection.label}`)
+            ))
+        });
+        quickPick.show();
+    }));
 
 
     vscode.commands.executeCommand(
