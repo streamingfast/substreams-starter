adf65d41268f4d2c589550dda0541734b5cded9f
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2024-08-01 16:09:56 -0400

    fixes on substreams download

--- .devcontainer/substreams/install.sh
+++ .devcontainer/substreams/install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#LINK=$(curl -s https://api.github.com/repos/streamingfast/substreams/releases/latest | awk "/download.url.*linux_$(uname -m | sed s/aarch64/arm64/)/ {print \$2}" | sed 's/"//g')
-#echo Downloading $LINK
-#curl -L  $LINK  | tar zxf -
-#mv substreams /usr/bin/substreams
-
-pushd /tmp
-  git clone https://github.com/streamingfast/substreams
-  cd substreams
-  git checkout -b codegen-cmd origin/codegen-cmd
-  go install -v ./cmd/substreams
-popd
+curl -LO  https://storage.googleapis.com/substreams-registry/bin/substreams
+chmod +x substreams
+mv substreams /usr/bin/
