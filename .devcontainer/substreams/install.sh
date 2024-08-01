1f63e0ecb1f7986ee79cadf115e53cd0ceb8eddd
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2024-08-01 15:37:54 -0400

    put back the substreams go build step

--- .devcontainer/substreams/install.sh
+++ .devcontainer/substreams/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-LINK=$(curl -s https://api.github.com/repos/streamingfast/substreams/releases/latest | awk "/download.url.*linux_$(uname -m | sed s/aarch64/arm64/)/ {print \$2}" | sed 's/"//g')
-echo Downloading $LINK
-curl -L  $LINK  | tar zxf -
-mv substreams /usr/bin/substreams
+#LINK=$(curl -s https://api.github.com/repos/streamingfast/substreams/releases/latest | awk "/download.url.*linux_$(uname -m | sed s/aarch64/arm64/)/ {print \$2}" | sed 's/"//g')
+#echo Downloading $LINK
+#curl -L  $LINK  | tar zxf -
+#mv substreams /usr/bin/substreams
 
-#pushd /tmp
-#  git clone https://github.com/streamingfast/substreams
-#  cd substreams
-#  git checkout -b codegen-cmd origin/codegen-cmd
-#  go install -v ./cmd/substreams
-#popd
+pushd /tmp
+  git clone https://github.com/streamingfast/substreams
+  cd substreams
+  git checkout -b codegen-cmd origin/codegen-cmd
+  go install -v ./cmd/substreams
+popd
