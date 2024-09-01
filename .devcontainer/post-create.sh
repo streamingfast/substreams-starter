71c82453cc9c0e1018df5f981c740fce76f35a9e
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2024-09-01 18:06:35 +0000

    add workspace as a 'safe.directory'

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,4 +6,5 @@ echo "if [ -f /workspace/.env ]; then source /workspace/.env; fi" >> ~/.bashrc
 echo "set +o allexport" >> ~/.bashrc
 
 echo "if [ -f /workspace/.substreams.env ]; then source /workspace/.substreams.env; fi" >> ~/.bashrc
+git config --global --add safe.directory /workspace
 
