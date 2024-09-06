afe09d81581b5b2962e69bebda6708cd363f3a6d
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2024-09-06 14:58:57 -0400

    bump substreams cli to v1.10.2

--- .devcontainer/substreams/install.sh
+++ .devcontainer/substreams/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -o substreams -L https://storage.googleapis.com/substreams-registry/bin/substreams-d8c21c9
+curl -o substreams -L https://storage.googleapis.com/substreams-registry/bin/substreams-v1.10.2
 chmod +x substreams
 mv substreams /usr/bin/
