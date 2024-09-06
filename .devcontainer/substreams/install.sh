25db103d4f19addcb27e8af277842ab210045642
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2024-09-06 13:44:52 -0400

    bump substreams image to d8c21c9 (v1.10.1)

--- .devcontainer/substreams/install.sh
+++ .devcontainer/substreams/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -o substreams -L https://storage.googleapis.com/substreams-registry/bin/substreams-4a35919
+curl -o substreams -L https://storage.googleapis.com/substreams-registry/bin/substreams-d8c21c9
 chmod +x substreams
 mv substreams /usr/bin/
